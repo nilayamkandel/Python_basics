085e7a4838eb8c592e390c238a4cbfd78e078df2
Neelayam-Kandel <neelayamdottkandel@gmail.com>
2023-01-21 11:20:50 +0545

    learn:__init__,self

--- oop.py
+++ oop.py
@@ -10,4 +10,16 @@ watch.alarm()                 #object.method to get the output
 mobile.alarm(watch)           #class.method(object)
 print(type(watch))
 
+# Special Method starts with __specialMethod__ i.e. __init__, __main__
+# Role of __init__ is, The __init__ method lets the class initialize 
+# the object's attributes and serves no other purpose. It is only used within classes.
+
+class mobile:
+    def __init__(self,memory,color):  #__init__ instantiate the object right when it is created
+        self.memory = memory     #we created an attribute of the class mobile
+        self.color= color
+        print(memory)
+samsung =mobile("5 gb","blue")
+iphone = mobile("1 tb","pink")
+
 
