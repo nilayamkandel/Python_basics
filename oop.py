42ad11486cab4e6070c9c2c4a2166826ecd3291c
Neelayam-Kandel <neelayamdottkandel@gmail.com>
2023-01-21 08:43:47 +0545

    learning-OOP-class,method,object

--- oop.py
+++ oop.py
@@ -1,3 +1,13 @@
-# class mob:
-#     def config:
+# " what is instanciating in python"
+# When a copy of a class is created that inherits all the class properties & functions, 
+# it is called instantiating a class. 
+
+class mobile:
+    def alarm(self):          #alarm() is the behaviour of mobile i.e. method
+        print("wake me at 5")
+watch = mobile()              #here watch is the object of mobile.
+watch.alarm()                 #object.method to get the output
+mobile.alarm(watch)           #class.method(object)
+print(type(watch))
+
 
