3426a99599e83e962f608136e500450c79534897
Neelayam-Kandel <neelayamdottkandel@gmail.com>
2023-01-24 11:41:58 +0545

    feat:supermethod

--- inheritance.py
+++ inheritance.py
@@ -3,6 +3,9 @@
 # also called base class. Child class is the class that inherits from another class,
 #also called derived class.MORE INFO ON "https://www.geeksforgeeks.org/inheritance-in-python/"
 
+
+
+
 # PROGRAM WITHOUT INHERITANCE
 class dog:
     def __init__(self,name,age):
@@ -22,6 +25,10 @@ class cat:
 
 d1 = dog("tommy",3)
 print(dog.get_name(d1))
+
+
+
+
 
 #PROGRAM WITH INHERITANCE
 class animals:
@@ -49,4 +56,40 @@ d1.show()
 # print(f"Here is my identity {d1.show()} and i can {d1.speak()}")
 
 
+# USE OF SUPER METHOD
+class animals:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def show(self):
+        print(f"I am {self.name} and I am {self.age}years old")
+
+    def speak(self):
+        print("i speak good language")
+
+class dog(animals):
+    def speak(self):
+        print("I can bark")
+
+    def __init__(self,name,age,color):
+        super().__init__(name,age,color)
+        self.color = color
+
+    def show(self):
+        print(f"I am {self.name} and I am {self.age}years old and i am {self.color}in color.")
+
+class cat(animals):
+    def speak(self):
+        print("I do meow")
+
+class fish(animals):
+    pass
+
+a = animals("tommy",5)
+a.show()
+d1 = dog("lucky",8,"brown")
+d1.show()
+f = fish("tim",3)
+f.speak()
 
